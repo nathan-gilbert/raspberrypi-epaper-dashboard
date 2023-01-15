c9b85bfcfae8ac4d090f028d8671b0a46e01e32f
Nathan Gilbert <nathan.gilbert@gmail.com>
2023-01-14 17:53:01 -0700

    Update to 3.11

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-
+from src.raspberrypi_epaper_dashboard.raspberrypi_dashboard \
+    import RaspberryPiEpaperDashboard
 
 if __name__ == "__main__":
-    pass
+    dashboard = RaspberryPiEpaperDashboard()
+    dashboard.update_epaper_display()
